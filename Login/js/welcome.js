72a31f2ed7ba975b87f82bee553715cca1d05d85
irawan-i <irawanhakim28@gmail.com>
2026-06-05 21:34:57 +0700

    Add files via upload

---
+++ Login/js/welcome.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+  function goLogin() {
+    window.location.href = "Login/index.html";
+  }
+
+  function logout() {
+    localStorage.removeItem("username");
+    location.reload();
+  }
+
+  const user = localStorage.getItem("username");
+
+  if (user) {
+    document.getElementById("userInfo").innerText = "Halo, " + user;
+
+    document.getElementById("authArea").innerHTML = `
+            <button onclick="logout()" class="nav-cta">Logout</button>
+        `;
+  }
+
+  window.goLogin = goLogin;
+  window.logout = logout;
+});
